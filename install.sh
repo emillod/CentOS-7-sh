7dafb42ff3827f9c13c353d91c09d6760b7d5717
emillod <emillo19525@gmail.com>
2017-01-04 21:19:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,3 +29,7 @@ echo "### SSH ###"
 echo "Rozpoczynam generowanie klucza SSH"
 ssh-keygen -t rsa
 echo "### KLUCZ ZOSTAŁ WYGENEROWANY ###
+sudo mv /etc/localtime /etc/localtime.bak
+sudo ln -s /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+echo "Zmieniono czas na:"
+date
