4a85946dd19bcd91eb7359a70455109016c0a2a6
emillod <emillo19525@gmail.com>
2017-01-04 23:36:02 +0100

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -1,27 +1,28 @@
-yum update -y
-yum install -y firewalld
-systemctl start firewalld.service
-firewall-cmd --state
-firewall-cmd --get-default-zone
-firewall-cmd --get-active-zones
-firewall-cmd --list-all
-firewall-cmd --get-zones
-firewall-cmd --zone=home --list-all
-systemctl restart firewalld.service
-firewall-cmd --get-active-zones
-systemctl restart network.service
-systemctl restart firewalld.service
-firewall-cmd --get-active-zones
-firewall-cmd --zone=public --add-service=http
-firewall-cmd --zone=public --add-service=https
-firewall-cmd --zone=public --add-service=ssh
-firewall-cmd --zone=public --permanent --add-service=http
-firewall-cmd --zone=public --permanent --add-service=https
-firewall-cmd --zone=public --permanent --add-service=ssh
-firewall-cmd --zone=public --add-port=10050/tcp
-firewall-cmd --zone=public --add-port=2641/tcp
+sudo yum install epel-release
+sudo yum update -y
+sudo yum install -y firewalld
+sudo systemctl start firewalld.service
+sudo firewall-cmd --state
+sudo firewall-cmd --get-default-zone
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --list-all
+sudo firewall-cmd --get-zones
+sudo firewall-cmd --zone=home --list-all
+sudo systemctl restart firewalld.service
+sudo firewall-cmd --get-active-zones
+sudo systemctl restart network.service
+sudo systemctl restart firewalld.service
+sudo firewall-cmd --get-active-zones
+sudo firewall-cmd --zone=public --add-service=http
+sudo firewall-cmd --zone=public --add-service=https
+sudo firewall-cmd --zone=public --add-service=ssh
+sudo firewall-cmd --zone=public --permanent --add-service=http
+sudo firewall-cmd --zone=public --permanent --add-service=https
+sudo firewall-cmd --zone=public --permanent --add-service=ssh
+sudo firewall-cmd --zone=public --add-port=10050/tcp
+sudo firewall-cmd --zone=public --add-port=2641/tcp
 sudo firewall-cmd --zone=public --permanent --add-port=10050/tcp
 sudo firewall-cmd --zone=public --permanent --add-port=2641/tcp
 echo "Poniżej znajdziesz listę wszystkich otwartych portów i usług:"
-firewall-cmd --zone=public --permanent --list-ports
-firewall-cmd --zone=public --permanent --list-services
+sudo firewall-cmd --zone=public --permanent --list-ports
+sudo firewall-cmd --zone=public --permanent --list-services
